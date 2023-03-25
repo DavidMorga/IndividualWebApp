d1c256244564dc52c19f6c852a77b014aa0ed4ab
DavidMorga <128841699+DavidMorga@users.noreply.github.com>
2023-03-26 01:25:58 +0400

    Add files via upload

---
+++ IndividualWebApp/activity.js
@@ -0,0 +1,93 @@
+let activity =[
+
+    {
+        "id": 1,
+        "title": "Math",
+        "location":"London",
+        "price": 150,
+        "image": "image/math.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 2,
+        "title": "English",
+        "location":"Chelsea",
+        "price": 150,
+        "image": "image/eng.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 3,
+        "title": "Physics",
+        "location":"Oxford",
+        "price": 80,
+        "image": "image/phy.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 4,
+        "title": "Music",
+        "location":"New York",
+        "price": 100,
+        "image": "image/music.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 5,
+        "title": "Web App",
+        "location":"Dubai",
+        "price": 150,
+        "image": "image/web.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 5,
+        "title": "UI/UX",
+        "location":"Sharjah",
+        "price": 90,
+        "image": "image/ui.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 7,
+        "title": "Math",
+        "location":"Dubai",
+        "price": 90,
+        "image": "image/math.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 8,
+        "title": "English",
+        "location":"Doha",
+        "price": 85,
+        "image": "image/eng.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 9,
+        "title": "Web App",
+        "location":"London",
+        "price": 100,
+        "image": "image/web.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id":10,
+        "title": "Economics",
+        "location":"New York",
+        "price": 100,
+        "image": "image/econs.jpg",
+        "availableInventory": 5
+       
+      }, 
+]
